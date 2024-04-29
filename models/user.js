d42fc647ba3c3e9aa2b279d7d3026933f4914c43
KongJian <kongjian3@hotmail.com>
2024-04-29 21:56:52 +0800

    Add: models/ userSchema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    minLength: 5,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("User, userSchema");
