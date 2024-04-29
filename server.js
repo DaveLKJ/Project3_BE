81c1480b4abe91ba74655700ff29dc19f035894a
KongJian <kongjian3@hotmail.com>
2024-04-29 21:33:51 +0800

    Add: database.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
-const favicon = require("serve-favicon");
-const logger = require("moragan");
+const logger = require("morgan");
 
 require("dotenv").config();
 
@@ -10,7 +9,6 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
 const PORT = 3001;
