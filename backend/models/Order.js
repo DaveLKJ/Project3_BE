b2989223c5f5e466c91579d16f32647b0fa2fc32
KongJian <kongjian3@hotmail.com>
2024-05-07 21:49:45 +0800

    add: models/orders lineItemSchema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,18 +1,36 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const itemSchema = require("./itemSchema");
 
-const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  shipped: { type: Boolean, default: false },
-  paymentStatus: {
-    type: String,
-    enum: ["pending", "paid", "failed"],
-    default: "pending",
+const lineItemSchema = new Schema(
+  {
+    qty: { type: Number, default: 1 },
+    item: itemSchema,
   },
-  paymentMethod: { type: String },
-  // other order details
-  timestamps: true,
-});
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
+
+const orderSchema = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    shipped: { type: Boolean, default: false },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+    paymentMethod: { type: String },
+    // other order details
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
 
 module.exports = mongoose.model("Order", orderSchema);
